2956deae9c2344f98444de594a472e54c0511af4
= <=>
2019-11-25 15:07:52 +0530

    #2 removed cdn

--- index1.php
+++ index1.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>HRD MANAGEMENT</title>
 
-    <!--  <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">-->
+
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
